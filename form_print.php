a7ada284a26fd18d13d71cd97b8c301bc3be4180
Surasak55 <74397708+Surasak55@users.noreply.github.com>
2020-11-28 22:00:33 +0700

    Add files via upload

---
+++ form_print.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="decorate.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reg - 206</title>
+    <style>
+    table, th, td {
+  border: 1px solid black;
+}</style>
+</head>
+<body>
+<!-- link -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- Other Script -->
+
+<!-- Pic and Detail -->
+<center><img src="pic/mfu.png" class="img-fluid" alt="mfulogo"></center>
+
+<form>
+<div class="container">
+
+
+<!-- Navbar -->
+<div id="bb">
+  <p> REGISTRAR DIVISION, MAE FAH LUANG UNIVERSITY </p>
+</div>
+<div id="bb">
+  <p> Request Form for Register More/Less Credits than those Stipulated </p>
+</div>
+<div class="container">
+
+<div style="text-align: center;">
+
+<label style="font-size=10px">Semester</label>
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+
+<label style="font-size=10px">First</label>
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+<label style="font-size=10px">Second</labelp>
+
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+<label style="font-size=10px">Summer</label>
+
+<label style="font-size=10px">Academic year...............................</label>
+</div>
+<br>
+
+<div>
+<p for="">(1) To the Dean of School......................................................................................................................................................................................................................................................</p>
+</div>
+</br>
+<!-- Student ID -->
+<div style="text-align: center;">
+<p for="">Name Mr / Miss / Mrs ...........................................................................................................................................</p>
+<p for="">Student ID </p>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+<canvas id="myCanvas" width="20" height="20"
+style="border:2px solid black;">
+</canvas>
+</div>
+
+</br>
+
+<div>
+<p>Study in School of.......................................................................................................................................................................................................................................................................</p>
+<p>Program of.....................................................................................................................................................................................................................................................................................</p>
+<p>Mobile phone...............................................................................................................................................................................................................................................................................</p>
+</div>
+
+<div class="container">
+<p>Cumulative grade piont average (GPAX)......................................................</p>
+<p">Student status</p>
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+<p"> Normal </p>
+
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+<p">Probation No.................</p>
+</div>
+
+</br>
+
+<div>
+<p>have registrered in the current semester for...............................................</p>
+<p>credits and would to alter the registered credits to ...................................................</p>
+<p>credits which makes the total credits registered</p>
+</div>
+
+<div>
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+<p">Less than 9 credits</p>
+</div>
+<div>
+<canvas id="myCanvas" width="15" height="15"
+style="border:2px solid black;">
+</canvas>
+<p">More than 22 credits</p>
+</div>
+<br>
+
+<div>
+<p">Reason of request :</p>
+<p">..........................................................................................................................................................................................................................................................................................................</p>
+<p">List of courses that make the total credits exceed the credits allowed</p>
+</div>
+
+<!-- Table -->
+<table border="10">
+  <tr>
+    <th colspan="7" width="300px" height="50px" style="text-align: center;">Course code</th>
+    <th width="750px" style="text-align: center;">Course title</th>
+    <th style="text-align: center;">Section</th>
+    <th width="100px" style="text-align: center;">Credits</th>
+  </tr>
+  <tr width="200px">
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+  </tr>
+
+  <tr width="200px">
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+    <td width="30px" height="45px"></td>
+  </tr>
+  </table>
+
+  <div>
+  <p">Remark : After 3 working days of the submission of this request, I will check whether the request has been approved at the Division of Registrar or via http://reg.mfu.ac.th</p>
+  </div>
+  <br><br>
+  
+  <div>
+  <p style="text-align: right; font-size: 20px;"> Student's Signature</p> 
+  </div>
+  <div>
+  <p style="text-align: right; font-size: 20px;">...................................................................................</p> 
+  </div>
+  <div>
+  <p style="text-align: right; font-size: 20px;">(.................................................................................)</p>
+  </div>
+
+  <div>
+  <p style="text-align: right; font-size: 20px;">............................/.........................../...............................</p>
+  </div>
+  <br>
+
+  <div class="row">
+          <div class="col-12 col-sm-6">
+              <h6>(2) Advisor's Comment</h6>
+              <br>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;...................................................................</p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;...................................................................</p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Signature &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;........................................................ </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; (............................................................................) </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; ................../........................./................... </p>
+          </div>
+          <div class="col-12 col-sm-6">
+              <h6>(4) Registrar Division's staff</h6>
+              <br>
+              <p class="">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <canvas id="myCanvas" width="15" height="15" style="border:2px solid black;"></canvas> Recorded </p>
+              <br>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Signature &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;........................................................ </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; (............................................................................) </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; ................../........................./................... </p>
+          </div>
+          <br>
+          
+          <div class="col-12 col-sm-6">
+              <h6>(3) Dean's Comment</h6>
+              <br>
+              <p class=""><canvas id="myCanvas" width="15" height="15" style="border:2px solid black;"></canvas>
+              Approved &nbsp; &nbsp; <canvas id="myCanvas" width="15" height="15" style="border:2px solid black;"></canvas>
+              Disapproved because................................</p>
+              <br>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Signature &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;........................................................ </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; (............................................................................) </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; ................../........................./................... </p>
+          </div>
+          <div class="col-12 col-sm-6">
+              <h6>(5) Head of the Registrar Division's comment</h6>
+              <br>
+              <p class="">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <canvas id="myCanvas" width="15" height="15" style="border:2px solid black;"></canvas>
+               Acknowledged </p>
+              <br>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Signature &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;........................................................ </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; (............................................................................) </p>
+              <p class=""> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; ................../........................./................... </p>
+          </div>
+          <br>
+
+  <div>
+</div>
+
+          </div>
+
+</div>
+</div>
+</form>
+
+<div class="container">
+<input type="button" name="Button" value="Print" onclick="javascript:this.style.display='none';window.print();" class="btn btn-primary">
+</div>
+
+<!-- Script -->
+<script>
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.fillStyle="white";
+ctx.fillRect(0,0,150,75);
+</script>
+
+<script>
+  cntx.font = 'italic 40px Calibri';
+  cntx.fillText(str, x, y);
+</script>
+
+</body>
+</html>
